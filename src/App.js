4495d888c07d49d054a07eedab15384be66c86da
santhosh162000 <santhosh.m@codingmart.com>
2021-04-05 17:38:51 +0530

    Added Navbar by using necessary image, text and font and hover option also

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <div className="bg"/>
-      <div className="content"></div>
+      <div className="content">
+        <Navbar />
+      </div>
       <div className="animations"></div>
       <div className="backhills"></div>
     </div>
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div class="Navbar">
+            <div id="oNavSpacer"></div>
+		    <nav>
+				<ul>
+                    <li>MY WORK</li>
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                    <li>SHOP</li>
+			    </ul>
+		    </nav>
+        </div>
+    )
+}
+
+export default Navbar;
