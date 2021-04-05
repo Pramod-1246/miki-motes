85a01d6d4bc554a6f4709039d3a583adb97faf3f
up2tech <68456969+up2tech@users.noreply.github.com>
2021-04-05 18:37:41 +0530

    Added features section with grids

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Features from './Features';
 
 function App() {
   return (
     <div>
       <div className="bg"/>
-      <div className="content"></div>
+      <div className="content">
+        <Features />
+      </div>
       <div className="animations"></div>
       <div className="backhills"></div>
     </div>
