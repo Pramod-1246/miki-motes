b5d75c552f2eabace5af7acd7e90d7d71df5517f
pramod <pramodjnrdhn@gmail.com>
2021-04-05 19:36:19 +0530

    ADDED POSTIOIONS TO THE ABOUT COMPONENT TO PLACE IT IN CORRECT POSITION

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Mywork from './Components/Mywork/Mywork'
+import Mywork from './Components/Mywork/Mywork';
+import AboutComponent from './Components/AboutComponent/AboutComponent';
+
 function App() {
   return (
     <div>
       <div className="bg"/>
       <div className="content">
         <Mywork/>
+        <AboutComponent />
       </div>
       <div className="animations"></div>
       <div className="backhills"></div>
