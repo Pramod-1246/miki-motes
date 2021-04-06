f19192d2e2e4613ed1597ed7bb45ddc18e95d6f8
Dhanavel <dhanavel@codingmart.com>
2021-04-06 09:21:31 +0530

    added loading gif on windows on load function for showing loader in home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Mywork from './Components/Mywork/Mywork';
 import AboutComponent from './Components/AboutComponent/AboutComponent';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    window.onload = function(){
+      document.getElementById('loader').style.display = 'none'
+    }
+  })
   return (
     <div>
+      <div className="loader" id="loader">
+        <div className="loader_div">
+          <img src="http://www.mikimottes.com/images/ajax-loader.gif" alt="Loading..."></img>
+        </div>
+      </div>
       <div className="bg"/>
       <div className="content">
         <Mywork/>
