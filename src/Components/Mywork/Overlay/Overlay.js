58246e41e98de41cdd70f70691fd18d3ca9238e8
Bala Yogesh <balayogesh2000@gmail.com>
2021-04-07 13:19:26 +0530

    Added Lightbox for Mywork section which containes a backdrop, header, image carousel,  and an image selector section

--- src/Components/Mywork/Overlay/Overlay.js
+++ src/Components/Mywork/Overlay/Overlay.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 import Backdrop from "../../Common/Backdrop/Backdrop";
+import CarouselIcon from "./CarouselIcon/CarouselIcon";
 
 import "./Overlay.css";
 
+const data = [
+  { imgSrc: "http://www.mikimottes.com/images/samples/cement_cats_01_big.jpg" },
+  { imgSrc: "http://www.mikimottes.com/images/samples/cement_cats_02_big.jpg" },
+  { imgSrc: "http://www.mikimottes.com/images/samples/cement_cats_04_big.jpg" },
+];
+
 const Overlay = (props) => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
     <>
       <Backdrop clicked={props.toggleOverlay} />
@@ -15,6 +27,10 @@ const Overlay = (props) => {
             <p>previous project</p>
           </div>
           <div className="overlay-header-center">
+            <img
+              src="http://www.mikimottes.com/images/lb_title.png"
+              alt="header"
+            />
             <h3>The Cement Cats</h3>
             <p>
               The cement cats are 100% handmade of pure cement, they just sit in
@@ -29,45 +45,48 @@ const Overlay = (props) => {
         </div>
         <div className="overlay-body">
           <Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
             intreval={null}
             fade={true}
             indicators={false}
             nextIcon={
-              <div
-                style={{
-                  width: "115px",
-                  height: "115px",
-                  background:
-                    "url('http://www.mikimottes.com/images/lb_next.png') no-repeat",
-                }}
-              ></div>
+              <CarouselIcon
+                src="http://www.mikimottes.com/images/lb_next.png"
+                iconType="next"
+              />
             }
             prevIcon={
-              <div
-                style={{
-                  width: "115px",
-                  height: "115px",
-                  background:
-                    "url('http://www.mikimottes.com/images/lb_previous.png') no-repeat",
-                }}
-              ></div>
+              <CarouselIcon
+                src="http://www.mikimottes.com/images/lb_previous.png"
+                iconType="prev"
+              />
             }
           >
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="http://www.mikimottes.com/images/samples/cement_cats_01_big.jpg"
-                alt="First slide"
-              />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="http://www.mikimottes.com/images/samples/cement_cats_04_big.jpg"
-                alt="Third slide"
-              />
-            </Carousel.Item>
+            {data.map((item) => {
+              return (
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={item.imgSrc}
+                    alt="slide"
+                  />
+                </Carousel.Item>
+              );
+            })}
           </Carousel>
+        </div>
+        <div className="overlay-bottom">
+          {data.map((item, idx) => {
+            return (
+              <div
+                className="overlay-bottom-img"
+                onClick={(e) => handleSelect(idx, e)}
+              >
+                <img src={item.imgSrc} alt={`${idx}`} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
