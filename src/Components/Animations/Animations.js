101df263f48f6c3261c557f06c7bdf7163405e39
pramod <pramodjnrdhn@gmail.com>
2021-04-07 18:35:34 +0530

    ADDED POOP ANIMATIONS WITHOUT ROTATING ANIMATION

--- src/Components/Animations/Animations.js
+++ src/Components/Animations/Animations.js
@@ -3,7 +3,13 @@ import './Animations.css'
 import Hat from './Hat/Hat'
 import Shopstarts from './Shopstarts/Shopstars'
 import ImageAnim from '../Common/ImageAnim/ImageAnim'
+import PoopAnim from '../Common/PoopAnim/PoopAnim';
+import { poopAnimDataArray } from '../Common/PoopAnim/PoopAnim'
+
 export default function Animations() {
+
+    
+
     return (
         <>
             <div className="light"></div>
@@ -17,6 +23,22 @@ export default function Animations() {
             <Shopstarts/>
             <ImageAnim name="pot_cover" top="1826px" marginLeft="-775px" width="74px" height="138px"
                     background="url(http://www.mikimottes.com/images/illustrations/all.png) no-repeat -1332px 0"/>
+            <ImageAnim name="poop_cover" top="626px" marginLeft="745px" width="19px" height="136px" 
+                    background="uurl(http://www.mikimottes.com/images/illustrations/all.png) no-repeat -1303px 0"/>
+            {
+                poopAnimDataArray.map( 
+                    poopAnimData => <PoopAnim 
+                                        id={poopAnimData.id}
+                                        top={poopAnimData.top} 
+                                        marginLeft={poopAnimData.marginLeft} 
+                                        imageOffset={poopAnimData.imageOffset}
+                                        originX={poopAnimData.originX}
+                                        originY={poopAnimData.originY}
+                                        poopAnimDirectionClass={poopAnimData.poopAnimDirectionClass}
+                                        scrollHide={poopAnimData.scrollHide}
+                                    />
+                    )
+            }
         </>
     )
 }
---
+++ src/Components/Common/PoopAnim/PoopAnim.js
@@ -0,0 +1,88 @@
+import './PoopAnim.css';
+import { useEffect } from 'react';
+import $ from 'jquery';
+
+
+function PoopAnim(props) {
+
+
+    useEffect( () => {
+        $(window).on('scroll', () => {
+            startPoopLoop();
+            if(window.pageYOffset > props.scrollHide){
+                $("#"+props.id).hide()
+            }
+            else{
+                $("#"+props.id).show();
+            }
+        })
+        // eslint-disable-next-line 
+    } , [])
+
+    const poopStyles = {
+        top: props.top,
+        marginLeft: props.marginLeft,
+        background: `url(http://www.mikimottes.com/images/illustrations/poop.png) no-repeat ${props.imageOffset} 0`,
+        transformOrigin: `${props.originX} ${props.originY}`
+    }  
+
+    return (
+        <div id={props.id} className="poop" style={poopStyles}></div>
+    );
+}
+
+export const poopAnimDataArray = [
+    {
+        id:"poop1",
+        top:"400px",
+        marginLeft:"745px",
+        imageOffset:"-60px",
+        originX:"10px",
+        originY:"5px",
+        poopAnimDirectionClass:".poop-anim-right",
+        scrollHide:220
+
+    },
+    {
+        id:"poop2",
+        top:"380px",
+        marginLeft:"745px",
+        imageOffset:"-45px",
+        originX:"10px",
+        originY:"5px",
+        poopAnimDirectionClass:".poop-anim-left",
+        scrollHide:240
+    },
+    {
+        id:"poop3",
+        top:"420px",
+        marginLeft:"747px",
+        imageOffset:"-30px",
+        originX:"10px",
+        originY:"5px",
+        poopAnimDirectionClass:".poop-anim-right",
+        scrollHide:200
+    },
+    {
+        id:"poop4",
+        top:"470px",
+        marginLeft:"748px",
+        imageOffset:"-15px",
+        originX:"9px",
+        originY:"5px",
+        poopAnimDirectionClass:".poop-anim-left",
+        scrollHide:140
+    },
+    {
+        id:"poop5",
+        top:"500px",
+        marginLeft:"750px",
+        imageOffset:"0",
+        originX:"6px",
+        originY:"5px",
+        poopAnimDirectionClass:".poop-anim-right",
+        scrollHide:116
+    }
+];
+
+export default PoopAnim;
